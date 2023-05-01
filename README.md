7848ad327630fca0ec985fe214acd627832426d6
Shreyas Sriram <shreyassriram3@gmail.com>
2023-05-01 10:40:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Harvard_CS50_Intro
+ Introduction to Computer Science @ Harvard https://cs50.harvard.edu/x/2023/
---
+++ lec1/mario .c
@@ -0,0 +1,10 @@
+int main (void)
+{
+     int n;
+
+     do
+     {
+          int n = get_int ("Height : ");
+     }
+     while ( n < 1 || n > 8)
+}
